7f4b3f659f51e53f8f3b865195e3da2f5cbb1d1c
Abhishek PM <codeabhishek18@gmail.com>
2024-10-14 09:38:28 +0530

    middleware fix

--- middleware.js
+++ middleware.js
@@ -6,47 +6,47 @@ import { adminRoutes, authRoutes, userRoutes } from "./routes";
 
 export default async function middleware(req)
 {
-    // const { nextUrl } = req
-    // const cookie = cookies()?.get('authjs.session-token');
+    const { nextUrl } = req
+    const cookie = cookies()?.get('__Secure-authjs.session-token');
     
-    // let user = null;
-    // if(cookie)
-    // {
-    //     user = await decode({
-    //         token: cookie.value,
-    //         salt: cookie.name,
-    //         secret: process.env.AUTH_SECRET
-    //     })
-    // } 
+    let user = null;
+    if(cookie)
+    {
+        user = await decode({
+            token: cookie.value,
+            salt: cookie.name,
+            secret: process.env.AUTH_SECRET
+        })
+    } 
 
-    // const userRoute = userRoutes.some((route)=> nextUrl.pathname.startsWith(route));
-    // const adminRoute = adminRoutes.some((route)=> nextUrl.pathname.startsWith(route));
-    // const authRoute = authRoutes.some((route)=> nextUrl.pathname.startsWith(route));
+    const userRoute = userRoutes.some((route)=> nextUrl.pathname.startsWith(route));
+    const adminRoute = adminRoutes.some((route)=> nextUrl.pathname.startsWith(route));
+    const authRoute = authRoutes.some((route)=> nextUrl.pathname.startsWith(route));
        
-    // if(user?.role === 'visitor' || !user )
-    //     if(userRoute || adminRoute)
-    //         return NextResponse.redirect(new URL('/login', nextUrl))
+    if(user?.role === 'visitor' || !user )
+        if(userRoute || adminRoute)
+            return NextResponse.redirect(new URL('/login', nextUrl))
 
-    // if(user?.role === 'visitor' && authRoute )
-    //     return NextResponse.redirect(new URL('/', nextUrl))
+    if(user?.role === 'visitor' && authRoute )
+        return NextResponse.redirect(new URL('/', nextUrl))
 
-    // if(user)
-    // {
-    //     if(user.role === 'user' && authRoute)
-    //         return NextResponse.redirect(new URL('/dashboard', nextUrl))
+    if(user)
+    {
+        if(user.role === 'user' && authRoute)
+            return NextResponse.redirect(new URL('/dashboard', nextUrl))
     
-    //     if(user.role !== 'admin' && nextUrl.pathname.startsWith('/admin'))
-    //         return NextResponse.redirect(new URL('/', nextUrl))
+        if(user.role !== 'admin' && nextUrl.pathname.startsWith('/admin'))
+            return NextResponse.redirect(new URL('/', nextUrl))
     
-    //     if(user.role === 'admin' && nextUrl.pathname.startsWith('/dashboard'))
-    //         return NextResponse.redirect(new URL('/admin/dashboard', nextUrl))
+        if(user.role === 'admin' && nextUrl.pathname.startsWith('/dashboard'))
+            return NextResponse.redirect(new URL('/admin/dashboard', nextUrl))
 
-    //     if(user.role === 'admin' && authRoute)
-    //         return NextResponse.redirect(new URL('/admin/dashboard', nextUrl))
+        if(user.role === 'admin' && authRoute)
+            return NextResponse.redirect(new URL('/admin/dashboard', nextUrl))
 
-    //     // if(nextUrl.pathname.startsWith('/admin'))
-    //     //     return NextResponse.redirect(new URL('/admin/dashboard', nextUrl))
-    // }
+        // if(nextUrl.pathname.startsWith('/admin'))
+        //     return NextResponse.redirect(new URL('/admin/dashboard', nextUrl))
+    }
 
     return null
     
